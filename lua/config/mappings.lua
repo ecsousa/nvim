a3dc6458d5a0f9e9538eac0870aa35cae622d730
Eduardo Sousa <ecsousa@gmail.com>
2025-02-23 23:45:49 -0800

    Refactored mappings to use lua

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -1,21 +1,28 @@
-vim.cmd[[
-vnoremap G G$
 
-nnoremap <silent> <SPACE> <SPACE>:noh<CR>
+local function noremap(mode, lhs, rhs, opts)
+  opts = opts or {}
+  opts.noremap=true
+  vim.keymap.set(mode, lhs, rhs, opts)
+end
 
-xnoremap > >gv
-xnoremap < <gv
+local function nnoremap(lhs, rhs, opts) noremap('n', lhs, rhs, opts) end
+local function vnoremap(lhs, rhs, opts) noremap('v', lhs, rhs, opts) end
+local function xnoremap(lhs, rhs, opts) noremap('x', lhs, rhs, opts) end
+local silent = { silent = true }
 
-nnoremap <silent> <TAB>n :tabnew<CR>
-nnoremap <silent> <TAB>x :tabclose<CR>
-nnoremap <silent> ]<TAB> :tabn<CR>
-nnoremap <silent> [<TAB> :tabp<CR>
-nnoremap <silent> [<S-TAB> :tabfirst<CR>
-nnoremap <silent> ]<S-TAB> :tablast<CR>
+vnoremap('G', 'G$')
 
-nnoremap <leader>a ggVG
+nnoremap('<SPACE>', '<SPACE>:noh<CR>')
+nnoremap('<TAB>n', ':tabnew<CR>')
+nnoremap('<TAB>x', ':tabclose<CR>')
+nnoremap(']<TAB>', ':tabn<CR>')
+nnoremap('[<TAB>', ':tabp<CR>')
+nnoremap('[<S-TAB>', ':tabfirst<CR>')
+nnoremap(']<S-TAB>', ':tablast<CR>')
 
-]]
+nnoremap('<leader>a', 'ggVG')
 
+xnoremap('>', '>gv', { noremap=true })
+xnoremap('<', '<gv', { noremap=true })
 
-vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename)
+nnoremap('<leader>r', vim.lsp.buf.rename)
